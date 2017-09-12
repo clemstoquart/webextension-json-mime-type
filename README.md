a15f98c755b4830ac23895eceed52b7144edcc0e
Clement Stoquart <clemstoquart@gmail.com>
2017-09-12 17:17:49 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JSON mime type override
+
+A simple web-extension that rewrites vendor specific json content-type value to the standard `application/json`.
+
+For example : `application/vnd.spring-boot.actuator.v1+json` will be modified into `application/json`.
---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function findJsonMimeType(header) {
+    return header.value.includes('json');
+}
+
+function overrideJsonHeader(request) {
+    return new Promise((resolve, reject) => {
+        if (request.responseHeaders.find(findJsonMimeType)) {
+            const jsonHeader = {
+                name: "Content-Type",
+                value: "application/json"
+            };
+            request.responseHeaders.push(jsonHeader);
+
+            resolve({responseHeaders: request.responseHeaders});
+        }
+    });
+}
+
+browser.webRequest.onHeadersReceived.addListener(
+    overrideJsonHeader,
+    {
+        urls: [ "<all_urls>" ]
+    },
+    [
+        "blocking",
+        "responseHeaders"
+    ]
+);
